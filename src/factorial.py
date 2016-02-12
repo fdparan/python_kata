98d94349aa27a1b76bc8ac8a2feb03c098ae738c
francisparan <francisparan@gmail.com>
2016-02-12 11:07:15 -0500

    factorial.py: Python 3 compliant by making use of reduce from functools

--- src/factorial.py
+++ src/factorial.py
@@ -5,13 +5,11 @@ Created on Wed Dec  2 20:54:52 2015
 @author: francisparan
 """
 
+from functools import reduce
+
 def factorial(num):
     if num < 0:
         raise ValueError('factorial() not defined for negative values')
 
-    result = 1
-    for n in range(result, num+1):
-        result *= n
-    return result
-
+    return reduce(lambda x, y: x*y, range(1, num+1), 1)
 
